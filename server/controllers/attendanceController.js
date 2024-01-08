080aeae54f7677b990ad4e517ded4e9eccd680b3
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-08 20:37:53 +0530

    controller updated

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -4,12 +4,12 @@ exports.addSubject = async (req, res) => {
     try {
         const subject = req.body;
 
-        const response = await attendanceServices.addNewSubject(subject)
+        const response = await attendanceServices.saveSubject(subject)
 
         console.log(response);
         res.status(200).json({ message: response });
     } catch (error) {
-        res.status(500).json({message: "internal server error"});
+        res.status(500).json({message: "internal server error", error});
     }
     
 }
