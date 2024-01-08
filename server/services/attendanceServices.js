d8918eec47b479feef608ce7f2f5e3c39dd84fd1
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-08 20:28:12 +0530

    updated save subject to include percentage

--- server/services/attendanceServices.js
+++ server/services/attendanceServices.js
@@ -1,11 +1,15 @@
 const SubjectModel = require('../models/attendance');
 
 exports.saveSubject = async (subject) => {
+
     const subjectToSave = new SubjectModel({
         name: subject.name,
         totalLectures: subject.totalLectures,
-        attendedLectures: subject.attendedLectures
+        attendedLectures: subject.attendedLectures,
+        percentage: subject.percentage
     });
 
     return subjectToSave.save()
 }
+
+exports.updateTotalLecute
