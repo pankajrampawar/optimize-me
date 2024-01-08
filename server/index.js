f104f6f0f7e131d1c503e13a357ef07d82c0bccf
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-08 20:38:25 +0530

    index.js updated to take in routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 
 const express = require('express')
-const app = express()
 const connectDb = require('./db')
+const attendanceRoutes = require('./routes/attendance');
+const bodyParser = require('body-parser');
+
 const port = 8080
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(bodyParser.json());
 
 connectDb();
+
+app.use('/attendance', attendanceRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
