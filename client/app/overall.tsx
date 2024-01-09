b692c4d1e4d58019686c9f8f351a8da912b8bb3e
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-10 05:29:28 +0530

    made components for differentiated attendance

--- client/app/overall.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Overall() {
-    return (
-        <div>
-            Overall attendance report
-        </div>
-    )
-}
--- client/app/subjectWise.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SubjectWise() {
-    return <div>
-        Subject wise is here
-    </div>
-}
