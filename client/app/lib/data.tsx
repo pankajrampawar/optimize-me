a17585ce216edece724c4fad5677596f8887345e
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-10 05:28:21 +0530

    udpated sidenav elements

---
+++ client/app/lib/data.tsx
@@ -0,0 +1,26 @@
+export const arrayOfSideNavElements = [
+    {
+        name: "Attendance",
+        location: " "
+    },
+    {
+        name: "Syllabus",
+        location: " ",
+    },
+    {
+        name: "Todo List",
+        location: " ",
+    },
+    {
+        name: "Assignments",
+        locaiton: " ",
+    },
+    {
+        name: "Second brain",
+        location: " ",
+    },
+    {
+        name: "Pomodoro",
+        location: " ",
+    }
+]
