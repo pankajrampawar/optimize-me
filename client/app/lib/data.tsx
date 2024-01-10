ef4c82e9b9110a23b231058356b8b7df1768287f
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-10 06:05:18 +0530

    updated element data to contain href link

--- client/app/lib/data.tsx
+++ client/app/lib/data.tsx
@@ -1,26 +1,32 @@
 export const arrayOfSideNavElements = [
     {
         name: "Attendance",
-        location: " "
+        location: " ",
+        href: "Attendance"
     },
     {
         name: "Syllabus",
         location: " ",
+        href: "Syllabus"
     },
     {
         name: "Todo List",
         location: " ",
+        href: "Todo"
     },
     {
         name: "Assignments",
         locaiton: " ",
+        href: "Assignment"
     },
     {
         name: "Second brain",
         location: " ",
+        href: "SecondBrain"
     },
     {
         name: "Pomodoro",
         location: " ",
+        href: "Pomodoro"
     }
 ]
