6c81e101346bbe7fbafb878e0a341956c13a865d
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-11 05:59:49 +0530

    svg file added

--- client/app/ui/attendance/overviewChip.tsx
+++ client/app/ui/attendance/overviewChip.tsx
@@ -1,9 +1,23 @@
 
 export default function OverviewChip() {
     return (
-        <div className="p-2 py-3 w-[330px] bg-CardColor rounded-2xl text-white font-light">
+        <div className="p-3 flex justify-between w-[330px] bg-CardColor rounded-2xl text-white font-light">
             <span className="tracking-wide">
                 Subject
+            </span>
+
+            <span>
+                40%
+            </span>
+
+            <span className="flex gap-1">
+                <button className="bg-TopColor">
+                    <img src="/add.svg" alt="add" className="p-1"/>
+                </button>
+                
+                <button className="bg-TopColor min-w-[20px]">
+                    <img src="/delete.svg" alt="trash" className="p-1"/>
+                </button>
             </span>
         </div>
     )
